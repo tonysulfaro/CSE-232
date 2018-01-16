1729546bf728afa00f2ab61dfe242a2470d38cc9
Tony Sulfaro <tony116523@gmail.com>
2018-01-15 22:12:37 -0500

    Fixed some mimir formatting

--- Projects/proj01/main.cpp
+++ Projects/proj01/main.cpp
@@ -9,33 +9,33 @@ int main(){
     //initialize variables
     double car_price;
     double sales_tax_rate;
-    double downpayment;
+    double down_payment;
     double title_and_fees;
     double yearly_interest;
     double month_duration;
     double monthly_rate;
 
     //get user input
-    cout << "Enter the car price.";
+    //cout << "Enter the car price.";
     cin >> car_price;
 
-    cout << "Enter the sales tax rate.";
+    //cout << "Enter the sales tax rate.";
     cin >> sales_tax_rate;
 
-    cout << "Enter the down payment.";
-    cin >> downpayment;
+    //cout << "Enter the down payment.";
+    cin >> down_payment;
 
-    cout << "Enter the title and fees.";
+    //cout << "Enter the title and fees.";
     cin >> title_and_fees;
 
-    cout << "Enter the yearly interest.";
+    //cout << "Enter the yearly interest.";
     cin >> yearly_interest;
 
-    cout << "Enter the Number of months for payment.";
+    //cout << "Enter the Number of months for payment.";
     cin >> month_duration;
 
     //calculate monthly payment
-    double monthly_payment_minus_fees = car_price - downpayment * ((monthly_rate * pow((1 + monthly_rate), month_duration)/ pow((1+ monthly_rate),month_duration-1)));
-    cout << "The monthly payment for the car is " << fixed << setprecision(2) << monthly_payment_minus_fees << endl;
+    double monthly_payment_minus_fees = car_price - down_payment * ((monthly_rate * pow((1 + monthly_rate), month_duration)/ pow((1+ monthly_rate),month_duration-1)));
+    cout << fixed << setprecision(2) << monthly_payment_minus_fees;
     return 0;
 }
