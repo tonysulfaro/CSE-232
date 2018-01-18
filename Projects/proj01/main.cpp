023927f5c95e533cf3c37d13d1108fcbfbb11ab1
Tony Sulfaro <tony116523@gmail.com>
2018-01-18 15:37:25 -0500

    forgot to add title and fees
    now it passes all mimir tests

--- Projects/proj01/main.cpp
+++ Projects/proj01/main.cpp
@@ -13,6 +13,7 @@ int main(){
     double title_and_fees;
     double yearly_interest;
     double month_duration;
+    double sales_tax_amount;
 
     //get user input
     cin >> car_price;
@@ -22,11 +23,18 @@ int main(){
     cin >> yearly_interest;
     cin >> month_duration;
 
-    double monthly_rate = yearly_interest/12;
+    //calculate other things after user input
+    double monthly_rate = yearly_interest/12.0;
+    sales_tax_amount = car_price*sales_tax_rate;
+
+    //add on other stuff to car price
+    car_price += sales_tax_amount;
+    car_price += title_and_fees;
 
     //calculate monthly payment
     double monthly_payment_minus_fees = (car_price - down_payment) *
-            ((monthly_rate * pow((1+monthly_rate),month_duration))/(pow((1+monthly_rate),month_duration)-1));
+            ((monthly_rate * pow((1+monthly_rate),month_duration))/
+                    (pow((1+monthly_rate),month_duration)-1));
 
     cout << fixed << setprecision(2) << monthly_payment_minus_fees;
     return 0;
