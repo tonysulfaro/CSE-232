046c4dbd6d1f63281b7834de6bbe6a7a22b7813e
Tony Sulfaro <tony116523@gmail.com>
2018-01-22 20:26:21 -0500

    added project 02
    did some work

---
+++ Projects/proj02/cmake-build-debug/CMakeFiles/proj02.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/proj02.dir/main.cpp.obj"
+  "proj02.pdb"
+  "proj02.exe"
+  "proj02.exe.manifest"
+  "libproj02.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/proj02.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Projects/proj02/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+//main function that juggles
+int main() {
+
+    long beginning;
+    long end;
+    int print_condition;
+
+    //get user input
+    cin >> beginning;
+    cin >> end;
+    cin >> print_condition;
+
+    //mainloop
+    for (beginning; beginning <= end; beginning++){
+
+        for (int i = beginning; beginning!= 1; beginning++){
+            cout << "ok";
+        }
+    }
+
+    return 0;
+}
