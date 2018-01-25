8e481cfa2491e8f67b03f852f2715ca75fd5a1c3
Tony Sulfaro <tony116523@gmail.com>
2018-01-25 16:00:14 -0500

    added proj02 test
    c++14 this time because 11 was being a pos

---
+++ Projects/proj02-test/cmake-build-debug/CMakeFiles/proj02_test.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/proj02_test.dir/main.cpp.obj"
+  "proj02_test.pdb"
+  "proj02_test.exe"
+  "proj02_test.exe.manifest"
+  "libproj02_test.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/proj02_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Projects/proj02-test/cmake-build-debug/CMakeFiles/proj02_test.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for proj02_test.
+# This may be replaced when dependencies are built.
