fe32e3e6aa696a04c77b18c9dfa2503496db513d
tonysulfaro <tony116523@gmail.com>
2018-01-30 10:26:26 -0500

    initial commit of lab03 base file

---
+++ Labs/lab03/lab03.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+
+int main(){
+
+    return 0;
+}
+
+//returns double of given evaluation
+int fn(double x){
+
+    return x;
+}
+
+//returns integral of a parameter
+int integral(double x){
+
+    return x;
+}
+
+//calculate area under curve fn
+int trapezoid(double point_a, double point_b, long trap_count){
+
+    double area_sum = 0;
+
+    return area_sum;
+}
