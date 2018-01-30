854f9176ba3eddc58481fea6521d7b14bca70882
Tony Sulfaro <tony116523@gmail.com>
2018-01-29 22:30:16 -0500

    intial commit of project 3 base files
    function templates as well

---
+++ Projects/proj03/cmake-build-debug/CMakeFiles/proj03.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/proj03.dir/main.cpp.obj"
+  "proj03.pdb"
+  "proj03.exe"
+  "proj03.exe.manifest"
+  "libproj03.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/proj03.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Projects/proj03/main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <string>
+
+using std::cout; using std::cin; using std::endl;
+
+//adds up all the divisors of a number
+int main() {
+
+    cout << "test";
+
+    return 0;
+}
+
+
+//return divisor sum
+long divisor_sum(long number){
+
+
+    long sum = 0;
+
+    return sum;
+}
+
+
+//returns greatest common divisor
+long gcd(long first_num, long second_num){
+
+    long greatest_divisor = 0;
+
+    return greatest_divisor;
+}
+
+//if arguments and divisor sum have their gcd 1
+bool is_solidary(long number){
+
+    bool condition = 0; //initialized to be false
+
+    return condition;
+}
+
+long friendly_check(long integrer_number, long upper_limit){
+
+    long adbIndex = 0;
+
+    return adbIndex;
+}
