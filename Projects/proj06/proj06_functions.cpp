d6cf108b8199c83e3a1b9bc58c7256505ea5ad28
tonysulfaro <tony116523@gmail.com>
2018-02-26 21:52:29 -0500

    initial commit of proj06 base files
    header, main, and function source files

---
+++ Projects/proj06/proj06_functions.cpp
@@ -0,0 +1,24 @@
+#include<string>
+using std::string;
+#include<vector>
+using std::vector;
+
+string vec_2_str(const vector<long>& v){
+
+    return v;
+}
+
+vector<long> gen_nstep_vector (long limit, long nstep){
+
+    return limit;
+}
+
+string num_to_nstep_coding(long num, long nstep){
+
+    return num;
+}
+
+long nstep_coding_to_num(const string& coding, const vector<long>& nstep_sequence){
+
+    return coding;
+}
---
+++ Projects/proj06/proj06_main.cpp
@@ -0,0 +1,10 @@
+#include<string>
+using std::string;
+#include<vector>
+using std::vector;
+#include "proj06_functions.h"
+
+int main(){
+
+    return 0;
+}
