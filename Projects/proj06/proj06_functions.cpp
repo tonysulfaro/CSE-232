ffe41eb76e2ea16c23743346ede2c76856ab8ffb
tonysulfaro <tony116523@gmail.com>
2018-02-28 19:14:45 -0500

    have vector to string working
    working on gen_nstep_vector

--- Projects/proj06/proj06_functions.cpp
+++ Projects/proj06/proj06_functions.cpp
@@ -1,24 +1,48 @@
 #include<string>
 using std::string;
 #include<vector>
-using std::vector;
+using std::vector; using std::to_string;
 
+#include<iostream>
+using std::cout; using std::endl;
+
+//convert vector contents to comma delimited string
 string vec_2_str(const vector<long>& v){
 
-    return v;
+    string s;
+
+    for(auto element: v){
+        s += to_string(element);
+        s += ",";
+    }
+    //truncate string if it has "," at the end
+    s = s.substr(0, s.size()-1);
+
+    return s;
 }
 
 vector<long> gen_nstep_vector (long limit, long nstep){
 
-    return limit;
+    vector<long> generated = {1,1};
+    int i = 0;
+    long next_element = 0;
+
+    while(next_element <= limit){
+        next_element = generated[i]+generated[i+1];
+        if(next_element <= limit){
+            generated.push_back(next_element);
+        }
+        i++;
+    }
+    return generated;
 }
 
 string num_to_nstep_coding(long num, long nstep){
 
-    return num;
+    return "string";
 }
 
 long nstep_coding_to_num(const string& coding, const vector<long>& nstep_sequence){
 
-    return coding;
+    return 100;
 }
--- Projects/proj06/proj06_main.cpp
+++ Projects/proj06/proj06_main.cpp
@@ -2,9 +2,19 @@
 using std::string;
 #include<vector>
 using std::vector;
+#include<iostream>
+using std::cout; using std::endl;
 #include "proj06_functions.h"
 
 int main(){
 
-    return 0;
+    //vec2str testing
+    string s = vec_2_str({2,3,4,3,5,6,6,64,2000});
+    //cout << s << endl;
+
+    //gen_nstep_vector testing
+    vector<long> v = gen_nstep_vector(100,2);
+    for(auto element: v){
+        cout << element << " " << endl;
+    }
 }
