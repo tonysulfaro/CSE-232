79d248265ab70f36de81ef850b059e6604c005b2
Tony Sulfaro <tony116523@gmail.com>
2018-03-12 13:42:42 -0400

    added lab08 base files

---
+++ Labs/lab08/lab08_main.cpp
@@ -0,0 +1,15 @@
+#include<vector>
+using std::vector;
+#include<string>
+using std::string;
+#include<map>
+using std::map;
+#include<utility>
+using std::pair;
+
+using Collatz = pair<long, vector<long> >;
+
+int main(){
+
+    return 0;
+}
