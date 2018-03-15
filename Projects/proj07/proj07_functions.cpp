bf343d790510a3a10cb101e9e1cbd3a9b3594403
tonysulfaro <tony116523@gmail.com>
2018-03-15 15:21:48 -0400

    actually got it to compile. neat

--- Projects/proj07/proj07_functions.cpp
+++ Projects/proj07/proj07_functions.cpp
@@ -46,34 +46,103 @@ server. Both of those conditions return true
 bool AddConnection(ServerData & sd, ServerName sn, UserName un){
 
     pair< map<string,set<string>>::iterator, bool> result;
-
     long key = sd.count(sn);
 
+    //if ServerName not in map
     if(key == 0){
         //make new server entry
         result = sd.insert({sn,{un}});
         return result.second;
     }
+    //existing server in map
     else{
-        //add user to existing host
+        //check if user is already in host
         for(auto person: sd[sn]){
             if(person == un){
                 return false;
             }
         }
+        //add user into host
         sd[sn].insert(un);
         return true;
     }
     return result.second;
 }
 bool DeleteConnection(ServerData &sd, ServerName sn, UserName un){
+
+    pair< map<string,set<string>>::iterator, bool> result;
+    long key = sd.count(sn);
+
+    //error handling: delete someone from non-existent server
+    if(key == 0){
+        return false;
+    }
+    //find the person then eliminate them
+    for(auto person: sd[sn]){
+        if(person == un){
+            sd[sn].erase(un);
+            return true;
+        }
+    }
+    //base case
     return false;
 }
 
-set<string> AllServers(const ServerData &sd);
-set<string> AllUsers(const ServerData &sd);
-set<string> HasConnections(const ServerData &sd, UserName un);
-set<string> HasUsers(const ServerData &sd, ServerName sn);
+//returns set of all servers in map
+set<string> AllServers(const ServerData &sd){
 
-void BalanceServers(ServerData &sd, ServerName sn1, ServerName sn2);
-void CleanAndBalance(ServerData &sd);
+    set<string> servers;
+    //for(auto server: sd){
+    //    servers.insert(server);
+    //}
+    return servers;
+}
+
+//returns set of all users in map
+set<string> AllUsers(const ServerData &sd){
+
+    set<string> users;
+    /*
+    for(auto server: sd){
+        for(auto user: server){
+            users.insert(user);
+        }
+    }
+    */
+    return users;
+}
+
+//returns set of all servers a user is in
+set<string> HasConnections(const ServerData &sd, UserName un){
+
+    set<string> connections;
+    /*
+    for(auto server: sd){
+        for(auto user: server){
+            if(user == un){
+                connections.insert(server); //this is alot of nesting
+            }
+        }
+    }
+    */
+    return connections;
+}
+
+//returns set of all users in a server
+set<string> HasUsers(const ServerData &sd, ServerName sn){
+
+    set<string> userList;
+    /*
+    for(auto user: sd[sn]){
+        userList.insert(user);
+    }
+    */
+    return userList;
+}
+
+void BalanceServers(ServerData &sd, ServerName sn1, ServerName sn2){
+    cout << "test";
+}
+void CleanAndBalance(ServerData &sd){
+    cout << "test";
+}
