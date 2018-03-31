e53de98beb558f37796a09a9a8f3d9d2c7017e24
tonysulfaro <tony116523@gmail.com>
2018-03-31 16:24:32 -0400

    added proj09 skeleton code
    constructor for mapset not complete still

--- Projects/proj09/proj09_mapset.cpp
+++ Projects/proj09/proj09_mapset.cpp
@@ -1,10 +1,60 @@
-#include<iostream>
+#include <iostream>
 using std::ostream;
-#include<string>
+#include <string>
 using std::string;
-#include<vector>
+#include <vector>
 using std::vector;
-#include<utility>
+#include <utility>
 using std::pair;
-#include<initializer_list>
+#include <initializer_list>
 using std::initializer_list;
+
+#include "proj09_mapset.h"
+
+/*default constructor
+MapSet::MapSet(initializer_list<pair<string, long>> data_list)
+{
+} */
+
+//update map
+bool MapSet::update(string name, long num)
+{
+	return false;
+}
+
+//remove a string
+bool MapSet::remove(string name)
+{
+	return false;
+}
+
+//add to map
+bool MapSet::add(pair<string, long> items)
+{
+	return false;
+}
+
+//compare items
+int MapSet::compare(MapSet &m)
+{
+	return 0;
+}
+
+//join MapSets together via union
+MapSet MapSet::mapset_union(MapSet &m)
+{
+	return m;
+}
+
+//join MapSets together via intersection
+MapSet MapSet::mapset_intersection(MapSet &m)
+{
+	return m;
+}
+
+//override << operator for printing mapset
+ostream &operator<<(ostream &out, MapSet &m)
+{
+	out << "Hello";
+	return out;
+}
--- Projects/proj09/proj09_mapset.h
+++ Projects/proj09/proj09_mapset.h
@@ -1,38 +1,38 @@
 #ifndef MAP_SET
 #define MAP_SET
 
-#include<iostream>
+#include <iostream>
 using std::ostream;
-#include<string>
+#include <string>
 using std::string;
-#include<vector>
+#include <vector>
 using std::vector;
-#include<utility>
+#include <utility>
 using std::pair;
-#include<initializer_list>
+#include <initializer_list>
 using std::initializer_list;
 
-class MapSet{
- private:
-  vector< pair<string,long> > v_;
-  vector< pair <string, long> >::iterator find_key(string);
- public:
-  MapSet() = default;
-  MapSet(initializer_list< pair<string,long> >);
+class MapSet
+{
+  private:
+	vector<pair<string, long>> v_;
+	vector<pair<string, long>>::iterator find_key(string);
 
-  size_t size();
-  pair<string,long> get(string);
-  bool update(string,long);
-  bool remove (string);
-  bool add(pair<string,long>);
-  int compare(MapSet&);
-  MapSet mapset_union (MapSet&);
-  MapSet mapset_intersection(MapSet&);
-  friend ostream& operator<<(ostream&, MapSet&);  
+  public:
+	MapSet() = default;
+	MapSet(initializer_list<pair<string, long>>);
 
+	size_t size();
+	pair<string, long> get(string);
+	bool update(string, long);
+	bool remove(string);
+	bool add(pair<string, long>);
+	int compare(MapSet &);
+	MapSet mapset_union(MapSet &);
+	MapSet mapset_intersection(MapSet &);
+	friend ostream &operator<<(ostream &, MapSet &);
 };
 
-ostream& operator<<(ostream&, MapSet&);
+ostream &operator<<(ostream &, MapSet &);
 
 #endif
-  
