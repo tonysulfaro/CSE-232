4ae05183b977cd6487766c32f7e78b37d37fdd83
tonysulfaro <tony116523@gmail.com>
2018-04-03 10:19:06 -0400

    initial commit of lab11 base files

---
+++ Labs/lab11/lab11_arrays.cpp
@@ -0,0 +1,29 @@
+#include<string>
+using std::string;
+#include<utility>
+using std::pair;
+#include<iostream>
+using std::ostream;
+
+#include "lab11_arrays.h"
+
+bool is_even(long l){
+	return false;
+}
+
+void print_array(long ary[], size_t sz, ostream& out){
+	std::cout << "test";
+}
+
+size_t fill_from_file(long* (& ary), string f_name){
+	size_t test = 0;
+	return test;
+}
+
+size_t concatenate(long* (& ary1), size_t sz1, long ary2[], size_t sz2){
+	return sz1;
+}
+
+pair<long*, size_t> copy_evens(long ary[], size_t sz){
+	return to_pair(1,sz);
+}
